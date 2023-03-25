8413e06b16e613d2453e117b8311aa28f1d2afd4
Lars Schieffer <lars@larsschieffer.de>
2023-03-25 22:20:40 +0100

    add imprint

--- app/routes/__content.tsx
+++ app/routes/__content.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/server-runtime";
 import Profile from "~/components/profile";
 
@@ -16,16 +16,19 @@ export default function ContentLayout() {
   const { copyright } = useLoaderData<typeof loader>();
   return (
     <div className="mx-6 mt-8 mb-2 md:mt-[198px]">
-      <div className="grid place-items-center ">
+      <div className="grid place-items-center">
         <div className="grid max-w-[1170px] gap-8 md:grid-flow-col">
           <Profile />
           <Outlet />
         </div>
       </div>
-      <p className="mx-auto mt-8 max-w-[1170px] rounded-xl bg-white/75 p-2 text-justify">
-        Copyright © {copyright.start}-{copyright.end} Lars Schieffer, All rights
-        reserved.
-      </p>
+      <div className="mx-auto mt-8 flex max-w-[1170px] flex-col justify-between rounded-xl bg-white/75 p-2 sm:flex-row">
+        <span>
+          Copyright © {copyright.start}-{copyright.end} Lars Schieffer, All
+          rights reserved.
+        </span>
+        <Link to="/imprint">Imprint</Link>
+      </div>
     </div>
   );
 }
---
+++ app/routes/imprint.tsx
@@ -0,0 +1,25 @@
+export default function Imprint() {
+  return (
+    <div className="grid h-screen place-items-center">
+      <div>
+        <h1>Imprint</h1>
+        <div className="flex content-center gap-8">
+          <p>
+            Contact: <br />
+            Lars Schieffer <br />
+            Neugasse 1a <br />
+            65719 Hofheim am Taunus <br />
+            Email: contact@larsschieffer.de
+          </p>
+          <p>
+            For editorial content: <br />
+            Responsible according to § 55 Abs.2 RStV <br />
+            Lars Schieffer <br />
+            Neugasse 1a <br />
+            65719 Hofheim am Taunus
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
