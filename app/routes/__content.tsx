98f675ce2332740ac78be64c89a08cf1159c47d1
Lars Schieffer <lars@larsschieffer.de>
2023-03-25 19:21:23 +0100

    add footer

--- app/routes/__content.tsx
+++ app/routes/__content.tsx
@@ -1,13 +1,31 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/server-runtime";
 import Profile from "~/components/profile";
 
+export function loader() {
+  const data = {
+    copyright: {
+      start: "2021",
+      end: new Date().toLocaleDateString("en-GB", { year: "numeric" }),
+    },
+  };
+  return json(data);
+}
+
 export default function ContentLayout() {
+  const { copyright } = useLoaderData<typeof loader>();
   return (
-    <div className="mx-6 my-8 grid place-items-center md:mt-[198px] md:mb-[130px]">
-      <div className="grid max-w-[1170px] gap-8 md:grid-flow-col">
-        <Profile />
-        <Outlet />
+    <div className="mx-6 mt-8 mb-2 md:mt-[198px]">
+      <div className="grid place-items-center ">
+        <div className="grid max-w-[1170px] gap-8 md:grid-flow-col">
+          <Profile />
+          <Outlet />
+        </div>
       </div>
+      <p className="mx-auto mt-8 max-w-[1170px] rounded-xl bg-white/75 p-2 text-justify">
+        Copyright © {copyright.start}-{copyright.end} Lars Schieffer, All rights
+        reserved.
+      </p>
     </div>
   );
 }
