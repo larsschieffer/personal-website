7939b351582dd4b950eec0faaf083a3ea9965bfa
Lars Schieffer <lars@larsschieffer.de>
2023-05-07 14:30:29 +0200

    update padding for imprint

--- app/routes/imprint.tsx
+++ app/routes/imprint.tsx
@@ -2,23 +2,27 @@ import RoundedBox from "~/components/rounded-box";
 
 export default function Imprint() {
   return (
-    <div className="grid h-screen place-items-center ">
+    <div className="mx-6 grid h-screen place-items-center">
       <RoundedBox>
-        <div className="flex content-center gap-8 bg-white p-8">
-          <p>
-            Contact: <br />
-            Lars Schieffer <br />
-            Neugasse 1a <br />
-            65719 Hofheim am Taunus <br />
-            Email: contact@larsschieffer.de
-          </p>
-          <p>
-            For editorial content: <br />
-            Responsible according to § 55 Abs.2 RStV <br />
-            Lars Schieffer <br />
-            Neugasse 1a <br />
-            65719 Hofheim am Taunus
-          </p>
+        <div className="flex flex-col content-center gap-8 bg-white p-8">
+          <div>
+            <span className="font-semibold">Contact:</span>
+            <p>
+              Lars Schieffer <br />
+              Neugasse 1a <br />
+              65719 Hofheim am Taunus <br />
+              Email: contact@larsschieffer.de
+            </p>
+          </div>
+          <div>
+            <span className="font-semibold"> For editorial content:</span>
+            <p>
+              Responsible according to § 55 Abs.2 RStV <br />
+              Lars Schieffer <br />
+              Neugasse 1a <br />
+              65719 Hofheim am Taunus
+            </p>
+          </div>
         </div>
       </RoundedBox>
     </div>
