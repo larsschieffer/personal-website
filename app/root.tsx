11653d1b3a83a14aa416d36e5edf07e0d1953d0d
Lars Schieffer <lars@larsschieffer.de>
2023-06-03 09:03:26 +0200

    add 404 route

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 import type { LinksFunction, V2_MetaFunction } from "@vercel/remix";
 import flatten from "flat";
@@ -13,6 +15,7 @@ import toastify from "react-toastify/dist/ReactToastify.css";
 import fonts from "~/styles/fonts.css";
 import tailwind from "~/styles/tailwind.css";
 import messages from "../public/assets/i18n/en.json";
+import { ErrorNotFound } from "./components/error/error-not-fount";
 import { Toast } from "./components/toast";
 import { metaFunctionFactory } from "./services/meta";
 
@@ -39,6 +42,37 @@ export const App = (): JSX.Element => {
           defaultLocale="en"
         >
           <Outlet />
+          <Toast />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </IntlProvider>
+      </body>
+    </html>
+  );
+};
+
+export const ErrorBoundary = (): JSX.Element => {
+  const error = useRouteError();
+  const isRouteError = isRouteErrorResponse(error);
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="font-inter text-gray-dark bg-gray-light  ">
+        <IntlProvider
+          messages={flatten(messages)}
+          locale="en"
+          defaultLocale="en"
+        >
+          <div className="w-screen h-screen max-h-screen max-w-screen grid place-items-center p-8">
+            {isRouteError ? <ErrorNotFound /> : <ErrorNotFound />}
+          </div>
           <Toast />
           <ScrollRestoration />
           <Scripts />
