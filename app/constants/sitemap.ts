039b932ae4da1920ead2049872518938098c1c0b
Lars Schieffer <contact@larsschieffer.de>
2023-06-08 20:26:06 +0200

    add rss feed

--- app/constants/sitemap.ts
+++ app/constants/sitemap.ts
@@ -1,6 +1,7 @@
 import type { SitemapUrl } from "~/types/sitemap";
 
-export const WEBSITE_URL = "https://www.larsschieffer.de/";
+export const DOMAINE = "larsschieffer.de";
+export const WEBSITE_URL = `https://${DOMAINE}/`;
 export const WEBSITE_PATHS: SitemapUrl[] = [
   { url: "about", changefreq: "yearly", priority: 0.5 },
   { url: "resume", changefreq: "monthly", priority: 0.5 },
