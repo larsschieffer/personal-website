af40f720fda452dc45417d3a5dfaaf408b1352a8
Lars Schieffer <contact@larsschieffer.de>
2023-06-11 12:26:16 +0200

    add redirect status code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { TypedResponse } from "@vercel/remix";
 import { redirect } from "@vercel/remix";
+import { StatusCodes } from "http-status-codes";
 
-export const loader = (): TypedResponse<never> => redirect("/about");
+export const loader = (): TypedResponse<never> =>
+  redirect("/about", StatusCodes.PERMANENT_REDIRECT);
